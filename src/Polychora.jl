2d5e2c26a313acec1af3baa12befc5b2bc9c5800
Nemo <nemo@debian>
2024-01-16 17:01:34 +0100

    Template copypaste

--- src/Polychora.jl
+++ src/Polychora.jl
@@ -1,5 +1,20 @@
 module Polychora
 
+# Using
+
+
+# Declare
+
 greet() = print("Hello World! What a fine day")
+
+ArithmeticSum(a₁,Δ,n) = return (n+1)*(a₁ + (a₁+n*Δ))/2
+
+SlowSum(a₁,Δ,n) = sum([a₁+Δ*i for i ∈ 0:n])
+
+# Export
+export ArithmeticSum
+
+# Includes
+#include("Reflections.jl")
 
 end # module Polychora
---
+++ test/runtests.jl
@@ -0,0 +1,11 @@
+using Polychora
+using Test
+
+SlowSum(a₁,Δ,n) = sum([a₁+Δ*i for i ∈ 0:n])
+
+@testset "ArithmeticSum" begin
+    @test ArithmeticSum(1,1,14) == SlowSum(1,1,14)
+    @test ArithmeticSum(5,1,10) == SlowSum(5,1,10)
+    @test ArithmeticSum(2,3,14) == SlowSum(2,3,14)
+    @test π ≈ 3.14 atol=0.01
+end
