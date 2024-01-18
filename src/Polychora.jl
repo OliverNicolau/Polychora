9b837843b88b089cdb74cffd8d1f8c490afa940a
Nemo <nemo@debian>
2024-01-18 14:51:41 +0100

    working tests

--- src/Polychora.jl
+++ src/Polychora.jl
@@ -32,32 +32,61 @@ function is_polytope_neighbourly(polytope)
     return is_fvector_neighborly(f_vector(polytope))
 end
 
-function moment_curve_polytope(d = 4, n = 10)
+function moment_curve_polytope(d,n)
     points = [[t^i for i in 1:d] for t in 1:n]
     polytope = convex_hull(points)
     return polytope
 end
 
-function random_polytope_test(d = 4, n = 20)
-    for rep in 0:10
-        polytope = rand_box_polytope(d, n, 1)
-        while nvertices(polytope) < n
-            polytope = convex_hull(polytope,convex_hull(rand(d)))
-        end
+function rand_box_polytope_exact(d,n)
+    polytope = convex_hull(rand(n,d))
+    while nvertices(polytope) < n
+        polytope = convex_hull(polytope,convex_hull(rand(d)))
+    end
+    return polytope
+end
+
+function parabolic_lift(points::Matrix)
+    lift(point) = sum(map((xᵢ) -> (xᵢ-10)^2,point))
+    lifted_coordinates = [lift(point) for point in eachrow(points)]
+    lifted_points = hcat(points,lifted_coordinates)
+    return convex_hull(lifted_points)
+end
+
+function parabolic_lift(polytope::Polyhedron)
+    points = Matrix(point_matrix(vertices(polytope)))
+    return parabolic_lift(points)
+end
+
+function random_polytope_test(d = 4, n = 20, rep = 10)
+    for iter in 0:rep
+        polytope = rand_box_polytope_exact(d,n)
         fvector = f_vector(polytope)
-        if is_fvector_neighborly(fvector) || true
-            #println("f⁰ = ", nvertices(polytope))
-            #println("fᵈ⁻¹ = ", nfacets(polytope))
-            println(fvector)
-            println(is_polytope_neighbourly(polytope))
+        #println("f⁰ = ", nvertices(polytope))
+        #println("fᵈ⁻¹ = ", nfacets(polytope))
+        println(fvector)
+        println(is_polytope_neighbourly(polytope))
+    end
+end
+
+function random_convex_lift_test(d = 4, n = 10, rep = 100)
+    for iter in 0:rep
+        polytope = parabolic_lift(parabolic_lift(rand(n,d-2)))
+        fvector = f_vector(polytope)
+        #println("f⁰ = ", nvertices(polytope))
+        #println("fᵈ⁻¹ = ", nfacets(polytope))
+        #println(fvector)
+        if is_polytope_neighbourly(polytope)
+            println(vertices(polytope))
         end
     end
 end
 
 # Export
-export random_polytope_test, moment_curve_polytope, is_fvector_neighborly
+export random_polytope_test, random_convex_lift_test
+export moment_curve_polytope, parabolic_lift, is_fvector_neighborly
 
 # Includes
-#include("Reflections.jl")
+#include("test.jl")
 
 end # module Polychora
